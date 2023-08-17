a0d64c47601886feef35d1ae92b06d43a62b902d
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-17 17:24:57 -0300

    Update SeparandoDigitos.py
    commitei novamente

--- SeparandoDigitos.py
+++ SeparandoDigitos.py
@@ -5,4 +5,16 @@ print('Unidade: {}'.format(num[3]))
 print('Dezena: {}'.format(num[2]))
 print('Centena: {}'.format(num[1]))
 print('Milhar: {}'.format(num[0]))
+
+print('\n','='*6, 'Separando digitos de um número - EX:2 ' ,'='*6)
+num = int(input('Digite um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando o número {}'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
 print('='*40)
