ecaa8eb830e431b66411e230b7b5560a47ed11de
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-17 19:51:55 -0300

    Create SeparandoStrings.py
    Separando o nome

---
+++ SeparandoStrings.py
@@ -0,0 +1,7 @@
+print('\n','='*10,' Separando a String nome ','='*10)
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}.'.format(nome[0]))
+print('Seu último nome é {}.'.format(nome[len(nome) - 1]))
+print('='*46)
