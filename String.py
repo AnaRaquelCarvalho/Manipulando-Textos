16fc12800a32b029038efa50b448da9a3cd977d3
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-17 19:20:57 -0300

    Create String.py
    Analisar quantas vezes a letra A apareceu na frase.

---
+++ String.py
@@ -0,0 +1,6 @@
+print('\n','='*10,' Analisando uma Strig ','='*10)
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra A apareceu {} vezes na frase.'.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {}.'.format(frase.find('A') + 1 ))
+print('A última letra A apareceu na posição {}.'.format(frase.rfind('A') + 1 ))
+print('='*46)
